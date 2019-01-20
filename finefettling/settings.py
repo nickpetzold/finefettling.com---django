f86900e7912aba9a0c911728101d468e0885cbf3
Nick Petzold <nickpetzold@gmail.com>
2019-01-20 11:55:36 +0000

    models set up

--- finefettling/settings.py
+++ finefettling/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import cloudinary
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +32,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary',
     'members_area.apps.MembersAreaConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,4 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+cloudinary.config(
+  cloud_name = "dy6wiutks",
+  api_key = "694278188222143",
+  api_secret = "ZcpF4y917uesuXOiUm2MNOMHCLM"
+)
 
